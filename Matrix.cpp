a49078ae7925cf6dda59d4fe9f95f785b3b5776b
nicrausaz <n.crausaz99@gmail.com>
2022-02-24 16:13:31 +0100

    random

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "Matrix.h"
 
-Matrix::Matrix(unsigned int rows, unsigned int cols) : rows(rows), cols(cols) {
+Matrix::Matrix(unsigned rows, unsigned int cols, unsigned modulo) : rows(rows), cols(cols), modulo(modulo) {
+
     data = new unsigned *[rows];
 
     for (unsigned i = 0; i < rows; ++i) {
@@ -10,12 +11,12 @@ Matrix::Matrix(unsigned int rows, unsigned int cols) : rows(rows), cols(cols) {
 
     for (unsigned i = 0; i < rows; ++i) {
         for (unsigned j = 0; j < cols; ++j) {
-            data[i][j] = 0;
+            data[i][j] = (1 + rand() / (RAND_MAX + 1.0) * modulo);
         }
     }
 }
 
-Matrix::Matrix(unsigned int rows) : Matrix(rows, rows) {}
+Matrix::Matrix(unsigned int rows, unsigned modulo) : Matrix(rows, rows, modulo) {}
 
 Matrix::~Matrix() {
 
--- Matrix.h
+++ Matrix.h
@@ -6,16 +6,18 @@
  * This class is used to represent a matrix.
  * @author Crausaz Nicolas & Maxime Scharwath
  */
+
 class Matrix {
 public:
-    Matrix (unsigned rows, unsigned cols);
-    Matrix(unsigned rows);
+    Matrix (unsigned rows, unsigned cols, unsigned modulo);
+    Matrix(unsigned rows, unsigned modulo);
 
     ~Matrix();
 
 private:
     const unsigned rows, cols;
     unsigned **data;
+    unsigned modulo;
 };
 
 
